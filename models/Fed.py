d72acc9b0fccf46d26a444a7e4a1e62e1f670e5e
zcl <z582977352@gmail.com>
2024-10-24 17:20:18 +0800

    Permutation-Entropy

--- models/Fed.py
+++ models/Fed.py
@@ -35,30 +35,81 @@ def getP(s_k, s_k_i):
     return s_k_i / sum_s_k
 
 
-def getEk(N_D, s_k):
-    sum = 0
-    for i in range(N_D):
-        p = getP(s_k, s_k[i])
-        if p == 0:
-            continue
-        sum += p * math.log(p)
-    return -1.0 * (1 / math.log(N_D)) * sum
-
+# def getEk(N_D, s_k):
+#     sum = 0
+#     for i in range(N_D):
+#         p = getP(s_k, s_k[i])
+#         if p == 0:
+#             continue
+#         sum += p * math.log(p)
+#     return -1.0 * (1 / math.log(N_D)) * sum
+def calculate_permutation_entropy(time_series, m=3, delay=1):
+    """
+    计算排列熵
+    time_series: 时间序列数据
+    m: 嵌入维度
+    delay: 时间延迟
+    """
+    n = len(time_series)
+    if n < m:
+        return 0
+    
+    # 生成所有可能的排列模式
+    patterns = {}
+    for i in range(n - (m-1)*delay):
+        # 获取长度为m的子序列
+        pattern = []
+        for j in range(m):
+            pattern.append(time_series[i + j*delay])
+        
+        # 获取排列顺序
+        sorted_idx = sorted(range(len(pattern)), key=lambda k: pattern[k])
+        pattern_str = ''.join(map(str, sorted_idx))
+        
+        # 统计模式出现次数
+        patterns[pattern_str] = patterns.get(pattern_str, 0) + 1
+    
+    # 计算每个模式的概率并计算熵
+    total_patterns = sum(patterns.values())
+    entropy = 0
+    for count in patterns.values():
+        p = count / total_patterns
+        entropy -= p * math.log(p)
+    
+    # 归一化
+    max_entropy = math.log(math.factorial(m))
+    return entropy / max_entropy if max_entropy != 0 else 0
 
 # 根据熵权法取得当前指标的权重
 def getWk(N_D, s, s_i):
-    sum = 0
+    """
+    使用排列熵计算权重
+    W_j = (1 - H_perm(X_j)) / Σ(1 - H_perm(X_i))
+    """
+    # 计算每个指标的排列熵
+    entropy = calculate_permutation_entropy(s_i, m=3)
+    
+    # 计算所有指标的排列熵之和
+    sum_entropy = 0
     for i in range(len(s)):
-        sum += getEk(N_D, s[i])
-    return (1 - getEk(N_D, s_i)) / (len(s) - sum)
+        sum_entropy += calculate_permutation_entropy(s[i], m=3)
+    
+    # 计算权重
+    if sum_entropy == 0:
+        return 1.0 / len(s)
+    return (1 - entropy) / (len(s) - sum_entropy)
 
 
 def getTauI(i, N_D, s):
-    sum = 0
+    """
+    计算综合得分
+    考虑时间序列的复杂度
+    """
+    sum_score = 0
     for k in range(len(s)):
-        sum += getWk(N_D, s, s[k]) * s[k][i]
-    return sum
-
+        weight = getWk(N_D, s, s[k])
+        sum_score += weight * s[k][i]
+    return sum_score
 
 # 根据牛顿冷却法取得当前模型的权重
 def getR(t, t0, theta, R0):
@@ -92,6 +143,15 @@ def normalization(s):
 
 
 def getAlpha(kexi, t, t0, theta, R0, i, N_D, s):
-    # 归一化解决时间戳数值过大导致的熵权过小的问题。
+    """
+    计算聚合权重
+    结合排列熵和时间衰减
+    """
+    # 归一化数据
     s = normalization(s)
-    return kexi * getR(t, t0, theta, R0) * getTauI(i, N_D, s)
+    
+    # 计算时间衰减和基于排列熵的权重
+    time_weight = getR(t, t0, theta, R0)
+    entropy_weight = getTauI(i, N_D, s)
+    
+    return kexi * time_weight * entropy_weight
